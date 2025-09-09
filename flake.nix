c33bfc4e4b3b7bdf9c554b2de27816d51e401f06
Eoghanmc22 <eoghanmc@icloud.com>
2025-09-09 12:32:55 -0400

    Attempt to use crane

--- flake.nix
+++ flake.nix
@@ -1,212 +1,262 @@
-# Run `nix run github:cargo2nix/cargo2nix` to update Cargo.nix
-# https://github.com/bevyengine/bevy/blob/v0.14.2/docs/linux_dependencies.md#nix
-# TODO: Export both nix packages and apps?
-# TODO: look into `workspaceShell` function, created by `makePackagSet`
+# See https://crane.dev/examples/quick-start-workspace.html
+#  - Look into the cargo-hakari stuff in this example, but i dont thing we need it
+# See https://crane.dev/examples/custom-toolchain.html
+# crane kinda sucks since we have to rebuild every dep if we change Cargo.toml
 {
-  description = "A Rusty ROV Control Software Stack";
+  description = "Build a cargo workspace";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    cargo2nix = {
-      url = "github:cargo2nix/cargo2nix/release-0.12";
-      inputs.rust-overlay.follows = "rust-overlay";
+
+    crane.url = "github:ipetkov/crane";
+
+    flake-utils.url = "github:numtide/flake-utils";
+
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
   outputs =
-    inputs@{
+    {
+      self,
       nixpkgs,
-      flake-parts,
+      crane,
+      flake-utils,
+      advisory-db,
       rust-overlay,
-      cargo2nix,
       ...
     }:
-      flake-parts.lib.mkFlake { inherit inputs; } {
-        systems = nixpkgs.lib.systems.flakeExposed;
-        perSystem = {self', pkgs, system, ...}:
-          let
-            rustToolchain = (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+
+        inherit (pkgs) lib;
+
+        craneLib = (crane.mkLib pkgs).overrideToolchain (
+          p:
+          (p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
               extensions = [ "rust-src" ];
               targets = [ "aarch64-unknown-linux-gnu" "armv7-unknown-linux-gnueabihf" ];
-            };
+            }
+        );
+        src = craneLib.cleanCargoSource ./.;
 
-            pkgs = import nixpkgs {
-              inherit system;
-              overlays = [inputs.cargo2nix.overlays.default (import inputs.rust-overlay)];
-            };
-            runtimeDeps = with pkgs;
-              (lib.optionals pkgs.stdenv.isLinux [
-                udev
-                alsa-lib
-                vulkan-loader
-                xorg.libX11
-                xorg.libXcursor
-                xorg.libXi
-                xorg.libXrandr # To use the x11 feature
-                libxkbcommon
-                wayland # To use the wayland feature
-              ])
-              ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
-                # https://discourse.nixos.org/t/the-darwin-sdks-have-been-updated/55295/1
-                apple-sdk_15
-              ])
-              ++ [
-                # Video streaming deps
-                gst_all_1.gstreamer
-                gst_all_1.gst-plugins-base
-                gst_all_1.gst-plugins-good
-                gst_all_1.gst-plugins-bad
-                gst_all_1.gst-plugins-ugly
-                gst_all_1.gst-libav
-                ffmpeg
+        depsSurface = with pkgs;
+          (lib.optionals pkgs.stdenv.isLinux [
+            pkg-config
+            udev
+            alsa-lib
+            vulkan-loader
+            xorg.libX11
+            xorg.libXcursor
+            xorg.libXi
+            xorg.libXrandr # To use the x11 feature
+            libxkbcommon
+            wayland # To use the wayland feature
+          ])
+          ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
+            # https://discourse.nixos.org/t/the-darwin-sdks-have-been-updated/55295/1
+            apple-sdk_15
+          ])
+          ++ [
+            # Video streaming deps
+            gst_all_1.gstreamer
+            gst_all_1.gst-plugins-base
+            gst_all_1.gst-plugins-good
+            gst_all_1.gst-plugins-bad
+            gst_all_1.gst-plugins-ugly
+            gst_all_1.gst-libav
+            ffmpeg
 
-                # opencv crate
-                libclang.lib
-                opencv
-                stdenv.cc.cc
-              ];
+            # opencv crate
+            clang
+            libclang.lib
+            opencv
+            stdenv.cc.cc
 
-            rustPkgs = pkgs.rustBuilder.makePackageSet {
-              inherit rustToolchain;
-              packageFun = import ./Cargo.nix;
-              packageOverrides = pkgs: pkgs.rustBuilder.overrides.all ++ [
-                (pkgs.rustBuilder.rustLib.makeOverride {
-                  # This needs to match the name of the crate the override should be applied to
-                  name = "surface";
-                  overrideAttrs = drv: {
-                    postFixup = ''
-                      patchelf --set-rpath ${pkgs.lib.makeLibraryPath runtimeDeps} $bin/bin/surface
-                    '';
-                  };
-                })
-                (pkgs.rustBuilder.rustLib.makeOverride {
-                  # This needs to match the name of the crate the override should be applied to
-                  name = "alsa-sys";
-                  overrideAttrs = drv: {
-                    propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [
-                      pkgs.alsa-lib
-                    ];
-                  };
-                })
-                # https://github.com/cargo2nix/cargo2nix/issues/241
-                # TODO: Find minimal overrides required
-                (pkgs.rustBuilder.rustLib.makeOverride {
-                  # This needs to match the name of the crate the override should be applied to
-                  name = "opencv";
-                  overrideAttrs = drv: {
-                    propagatedBuildInputs = drv.propagatedBuildInputs or [ ] ++ [
-                      pkgs.opencv
-                      pkgs.clang
-                      # pkgs.libclang.lib
-                      # pkgs.rustPlatform.bindgenHook
-                    ];
-                    nativeBuildInputs = with pkgs;
-                      drv.nativeBuildInputs or [ ] ++ [ pkg-config libclang.lib ];
-                    LIBCLANG_PATH =
-                      pkgs.lib.makeLibraryPath [ pkgs.llvmPackages.libclang.lib ];
-                    LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath runtimeDeps;
-                  };
-                })
-                (pkgs.rustBuilder.rustLib.makeOverride {
-                  # This needs to match the name of the crate the override should be applied to
-                  name = "bevy_render";
-                  overrideAttrs = drv: {
-                    # TODO: What to put here to fix build
-                  };
-                })
-              ];
-            };
-          in {
-            packages = rec {
-              surface = (rustPkgs.workspace.surface {}).bin;
-              default = surface;
-            };
-            devShells.default = pkgs.mkShell rec {
-              buildInputs = with pkgs; [
-                rustPlatform.bindgenHook
-                pkg-config
-              ] ++ runtimeDeps ++ [
-                rustToolchain
-                # rust-analyzer-nightly
-              ];
-              LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath buildInputs;
-            };
+            openssl.dev
+          ];
+
+
+        # Common arguments can be set here to avoid repeating them later
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          # TODO: nativeBuildInputs should only have build deps, and buildInputs should only have runtime deps
+          # TODO: I dont want depsSurface here since this will leak into the build for robot
+          nativeBuildInputs = depsSurface;
+          buildInputs = depsSurface ++ [
+            # Add additional build inputs here
+          ]
+          ++ lib.optionals pkgs.stdenv.isDarwin [
+            # Additional darwin specific inputs can be set here
+            pkgs.libiconv
+          ];
+          # TODO: I think we can get away with just setting LIBCLANG_PATH
+          LD_LIBRARY_PATH = lib.makeLibraryPath depsSurface;
+
+          # Additional environment variables can be set directly
+          # MY_CUSTOM_VAR = "some value";
+        };
+
+        # Build *just* the cargo dependencies (of the entire workspace),
+        # so we can reuse all of that work (e.g. via cachix) when running in CI
+        # It is *highly* recommended to use something like cargo-hakari to avoid
+        # cache misses when building individual top-level-crates
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        individualCrateArgs = commonArgs // {
+          inherit (craneLib.crateNameFromCargoToml { inherit src; }) version;
+          # NB: we disable tests since we'll run them all via cargo-nextest
+          doCheck = false;
+        };
+
+        fileSetForCrate =
+          crate:
+          lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./Cargo.toml
+              ./Cargo.lock
+              # TODO: Why is this needed?
+              (craneLib.fileset.commonCargoSources ./common)
+              (craneLib.fileset.commonCargoSources ./motor_math)
+              (craneLib.fileset.commonCargoSources ./networking)
+              (craneLib.fileset.commonCargoSources ./stable_hashmap)
+
+              # TODO: Binarie crates absolutely should not be here
+              (craneLib.fileset.commonCargoSources ./robot)
+              (craneLib.fileset.commonCargoSources ./surface)
+              (craneLib.fileset.commonCargoSources ./waterlinked)
+              # (craneLib.fileset.commonCargoSources crate)
+            ];
           };
-      };
 
-    # let
-    #   systems = [
-    #     "x86_64-linux"
-    #     "aarch64-linux"
-    #     "aarch64-darwin"
-    #   ];
-    #   # Helper function to generate a set of attributes for each system
-    #   forAllSystems = func: (nixpkgs.lib.genAttrs systems func);
-    # in
-    # {
-    #   devShells = forAllSystems (
-    #     system:
-    #     let
-    #       pkgs = import nixpkgs {
-    #         inherit system;
-    #         overlays = [ rust-overlay.overlays.default ];
-    #       };
-    #       lib = pkgs.lib;
-    #     in
-    #     {
-    #       default = pkgs.mkShell rec {
-    #         nativeBuildInputs = with pkgs; [
-    #           rustPlatform.bindgenHook
-    #           pkg-config
-    #           # lld is much faster at linking than the default Rust linker
-    #           lld
-    #         ];
-    #         buildInputs =
-    #           with pkgs;
-    #           [
-    #             # rust toolchain
-    #             ((rust-bin.fromRustupToolchainFile ./rust-toolchain.toml).override {
-    #               extensions = [ "rust-src" ];
-    #               targets = [ "aarch64-unknown-linux-gnu" "armv7-unknown-linux-gnueabihf" ];
-    #             })
-    #           ]
-    #           # https://github.com/bevyengine/bevy/blob/v0.14.2/docs/linux_dependencies.md#nix
-    #           ++ (lib.optionals pkgs.stdenv.isLinux [
-    #             udev
-    #             alsa-lib
-    #             vulkan-loader
-    #             xorg.libX11
-    #             xorg.libXcursor
-    #             xorg.libXi
-    #             xorg.libXrandr # To use the x11 feature
-    #             libxkbcommon
-    #             wayland # To use the wayland feature
-    #           ])
-    #           ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
-    #             # https://discourse.nixos.org/t/the-darwin-sdks-have-been-updated/55295/1
-    #             apple-sdk_15
-    #           ])
-    #           ++ [
-    #             # Video streaming deps
-    #             gst_all_1.gstreamer
-    #             gst_all_1.gst-plugins-base
-    #             gst_all_1.gst-plugins-good
-    #             gst_all_1.gst-plugins-bad
-    #             gst_all_1.gst-plugins-ugly
-    #             gst_all_1.gst-libav
-    #             ffmpeg
-    #
-    #             # opencv crate
-    #             libclang.lib
-    #             opencv
-    #             stdenv.cc.cc
-    #           ];
-    #         LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
-    #       };
-    #     }
-    #   );
-    # };
+        # Build the top-level crates of the workspace as individual derivations.
+        # This allows consumers to only depend on (and build) only what they need.
+        # Though it is possible to build the entire workspace as a single derivation,
+        # so this is left up to you on how to organize things
+        #
+        # Note that the cargo workspace must define `workspace.members` using wildcards,
+        # otherwise, omitting a crate (like we do below) will result in errors since
+        # cargo won't be able to find the sources for all members.
+        surface = craneLib.buildPackage (
+          individualCrateArgs
+          // {
+            pname = "surface";
+            # cargoExtraArgs = "-p my-cli";
+            src = fileSetForCrate ./surface;
+          }
+        );
+        # my-server = craneLib.buildPackage (
+        #   individualCrateArgs
+        #   // {
+        #     pname = "my-server";
+        #     cargoExtraArgs = "-p my-server";
+        #     src = fileSetForCrate ./crates/my-server;
+        #   }
+        # );
+      in
+      {
+        checks = {
+          # Build the crates as part of `nix flake check` for convenience
+          inherit surface;
+
+          # Run clippy (and deny all warnings) on the workspace source,
+          # again, reusing the dependency artifacts from above.
+          #
+          # Note that this is done as a separate derivation so that
+          # we can block the CI if there are issues here, but not
+          # prevent downstream consumers from building our crate by itself.
+          workspace-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
+
+          workspace-doc = craneLib.cargoDoc (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              # This can be commented out or tweaked as necessary, e.g. set to
+              # `--deny rustdoc::broken-intra-doc-links` to only enforce that lint
+              env.RUSTDOCFLAGS = "--deny warnings";
+            }
+          );
+
+          # Check formatting
+          workspace-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          workspace-toml-fmt = craneLib.taploFmt {
+            src = pkgs.lib.sources.sourceFilesBySuffices src [ ".toml" ];
+            # taplo arguments can be further customized below as needed
+            # taploExtraArgs = "--config ./taplo.toml";
+          };
+
+          # Audit dependencies
+          workspace-audit = craneLib.cargoAudit {
+            inherit src advisory-db;
+          };
+
+          # Audit licenses
+          workspace-deny = craneLib.cargoDeny {
+            inherit src;
+          };
+
+          # Run tests with cargo-nextest
+          # Consider setting `doCheck = false` on other crate derivations
+          # if you do not want the tests to run twice
+          workspace-nextest = craneLib.cargoNextest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+              cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+            }
+          );
+        };
+
+        packages = {
+          inherit surface;
+        };
+
+        apps = {
+          # Building this seems to also build robot and everyting else
+          surface = flake-utils.lib.mkApp {
+            drv = surface;
+          };
+        };
+
+        devShells.default = craneLib.devShell {
+          # Inherit inputs from checks.
+          checks = self.checks.${system};
+
+          # Additional dev-shell environment variables can be set directly
+          # MY_CUSTOM_DEVELOPMENT_VAR = "something else";
+
+          # Extra inputs can be added here; cargo and rustc are provided by default.
+          packages = [
+            # TODO: 
+          ] ++ depsSurface;
+        };
+      }
+    );
 }
 
